66a719e6b736613bed8179cd5159848c70ce81d8
Tobias Davino Purba <purbatobias@gmail.com>
2023-12-21 14:43:23 +0700

    Nothing for now

--- app/src/main/java/com/alterpat/budgettracker/MainActivity.kt
+++ app/src/main/java/com/alterpat/budgettracker/MainActivity.kt
@@ -1,1 +1,7 @@
-testing commit
+class MainActivity : AppCompatActivity() {
+    private lateinit var deletedTransaction: Transaction
+    private lateinit var transactions : List<Transaction>
+    private lateinit var oldTransactions : List<Transaction>
+    private lateinit var transactionAdapter: TransactionAdapter
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var db : AppDatabase
